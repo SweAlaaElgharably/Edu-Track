70f1448ce2a86cb787ae2e15c1afd2023f7c4005
Galal Elsayed <agalal3516@gmail.com>
2025-08-07 10:16:48 +0300

    ..

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from .models import User
 
 class UserSerializer(ModelSerializer):
@@ -9,6 +10,17 @@ class UserSerializer(ModelSerializer):
             "phonenumber", "birthday", "placeofbirth", "nationalid", "nationality", "zipcode", 
             "gender", "maritalstatus" 
         )
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        model = User
+        fields = ("id", "username", "first_name", "last_name", "email", "password", "re_password")
+
+    def create(self, validated_data):
+        print("Creating user with data:", validated_data)  # Debug print
+        user = super().create(validated_data)
+        print("Created user:", user.username, user.first_name, user.last_name)  # Debug print
+        return user
 
     #program
     #faculty
