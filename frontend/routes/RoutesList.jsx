d806d3569d80734731c1a50fe542c3c67a70452b
ziadKramzy <ziadramzy2@gmail.com>
2025-08-11 02:11:15 +0300

    aded fixes to router and dashboards css and app conditions

--- frontend/routes/RoutesList.jsx
+++ frontend/routes/RoutesList.jsx
@@ -15,6 +15,7 @@ import UniversityManage from "../src/pages/UniversityManage";
 import FacultyManage from "../src/pages/FacultyManage";
 import PageNotFound from "../src/pages/PageNotFound";
 import ForgotPassword from "../src/pages/ForgotPassword";
+import HelpCenter from "../src/pages/HelpCenter";
 
 const RoutesList = () => {
   return (
@@ -73,6 +74,8 @@ const RoutesList = () => {
           </ProtectedRoute>
         }
       />
+      <Route path="/help" element={<HelpCenter />} />
+
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
