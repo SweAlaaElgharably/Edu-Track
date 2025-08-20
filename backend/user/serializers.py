01e8a76d0a8332338928154979b2990ba84c94a7
Galal Elsayed <agalal3516@gmail.com>
2025-08-20 15:41:24 +0300

    uni,fac,program user updates..

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,16 +1,23 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User
+from university.serializers import UniversitySerializer
+from faculty.serializers import FacultySerializer
+from program.serializers import ProgramSerializer
 
 class UserSerializer(ModelSerializer):
+    university = UniversitySerializer(read_only=True)
+    faculty = FacultySerializer(read_only=True)
+    program = ProgramSerializer(read_only=True)
+
     class Meta:
         model = User
         fields = (
-            "username", "first_name", "last_name", "email", "englishfullname", "address", "religion", "picture", 
-            "phonenumber", "birthday", "placeofbirth", "nationalid", "nationality", "zipcode", 
-            "gender", "maritalstatus", "groups" 
+        "username", "first_name", "last_name", "email", "englishfullname", "address", "religion", "picture",
+        "phonenumber", "birthday", "placeofbirth", "nationalid", "nationality", "zipcode",
+        "gender", "maritalstatus", "groups",
+        # nested related objects for profile display
+        "university", "faculty", "program",
         )
 
-    #program
-    #faculty
-    #university
+    # The related fields are read-only; updates are handled via dedicated endpoints if needed.
 
