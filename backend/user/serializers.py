cb2928539a9eed024ff3f57ccc206d01299b8ff4
SweAlaaElgharably <swe.alaaElgharably@gmail.com>
2025-08-22 23:37:42 +0300

    add groups and logs

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User
+from django.contrib.auth.models import Group
+from django.contrib.admin.models import LogEntry
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -9,3 +11,15 @@ class UserSerializer(ModelSerializer):
             "phonenumber", "birthday", "placeofbirth", "nationalid", "nationality", "zipcode", 
             "gender", "maritalstatus", "groups", "program", "faculty", "university"
         )
+
+
+class GroupSerializer(ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ["id", "name", "permissions"]
+        depth = 1   
+        
+class LogSerializer(ModelSerializer):
+    class Meta:
+        model = LogEntry
+        fields = ["id", "action_time", "user", "content_type", "object_repr", "change_message", "action_flag"]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import Group
+from .serializers import GroupSerializer, LogSerializer
+from django.contrib.admin.models import LogEntry
 
 # Create your views here.
+class GroupList(generics.ListAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class LogList(generics.ListAPIView):
+    queryset = LogEntry.objects.all().order_by("-action_time")
+    serializer_class = LogSerializer
